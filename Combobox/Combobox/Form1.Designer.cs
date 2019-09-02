8d8d50d3091a1af2c47f04e56ad2cbcfabbc6e04
Alumno <Alumno@PC03>
2019-09-02 09:56:27 -0500

    dasdsasadsad

---
+++ Combobox/Combobox/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "Lunes",
+            "martes",
+            "Miercolés ",
+            "Jueves ",
+            "Viernes ",
+            "Sábado ",
+            "Domingo"});
+            this.cb1.Location = new System.Drawing.Point(29, 25);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(386, 21);
+            this.cb1.TabIndex = 0;
+            this.cb1.Text = "Qué día es hoy?";
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.Cb1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 320);
+            this.Controls.Add(this.cb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+    }
+}
+
---
+++ Combobox2/Combobox2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combobox2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add("Pozole");
+            cbAlimentos.Items.Add("Tostadas de pata");
+        }
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add(textBox1.Text);
+        }
+
+        private void CbAlimentos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = cbAlimentos.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = cbAlimentos.Items[indice].ToString();
+        }
+    }
+}
