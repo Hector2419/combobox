1180e6bdfbbfc82c7f66ffb53ce6286699b815c8
Alumno <Alumno@PC17>
2019-09-10 12:29:14 -0500

    2.0

--- Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
+++ Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -93,6 +94,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(75, 20);
             this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
             // 
             // menuStrip1
             // 
@@ -144,11 +146,23 @@
             this.textBox2.Size = new System.Drawing.Size(118, 20);
             this.textBox2.TabIndex = 10;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(106, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "&Transform";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(437, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
@@ -184,6 +198,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Proyecto 1.0/Proyecto 1.0/Form1.cs
+++ Proyecto 1.0/Proyecto 1.0/Form1.cs
@@ -12,8 +12,8 @@ namespace Proyecto_1._0
 
     public partial class Form1 : Form
     {
-        double coin, result;
-        string op;
+        double n;
+        string op, coin, result;
         public Form1()
         {
             InitializeComponent();
@@ -39,6 +39,257 @@ namespace Proyecto_1._0
         private void ArchivoToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double o,res;
+            string selectedItem = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            coin = comboBox1.Text;
+            op = comboBox2.Text;
+            n = float.Parse(textBox1.Text); 
+            if (coin == "Dollar")
+            {
+                if (op == "Dollar")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                }
+                else if (op== "Mexican Peso")
+                {
+                    o = 19.51;
+                    res = n*o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                }
+                else if (op == "Euro")
+                {
+                    o = 0.91;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                }
+                else if (op == "Yen")
+                {
+                    o = 107.43;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                }
+                else if (op == "Soles")
+                {
+                    o = 3.34;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+
+                }
+                else if (op == "Rupias")
+                {
+                    o = 71.87;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                }
+                else if (op == "Libra")
+                {
+                    o = 0.81;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                }
+            }
+            else if (coin == "Mexican Peso")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
+            else if (coin == "Euro")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
+            else if (coin == "Yen")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
+            else if (coin == "Soles")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
+            else if (coin == "Rupias")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
+            else if (coin == "Libra")
+            {
+                if (op == "Dollar")
+                {
+
+                }
+                else if (op == "Mexican Peso")
+                {
+
+                }
+                else if (op == "Euro")
+                {
+
+                }
+                else if (op == "Yen")
+                {
+
+                }
+                else if (op == "Soles")
+                {
+
+                }
+                else if (op == "Rupias")
+                {
+
+                }
+                else if (op == "Libra")
+                {
+
+                }
+            }
         }
 
         private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
