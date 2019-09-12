594ad17f380561962d3c4d872874486677f458b8
Alumno <Alumno@PC20>
2019-09-12 10:07:50 -0500

    4.1

--- Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
+++ Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
@@ -75,7 +75,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 190);
+            this.linkLabel1.Location = new System.Drawing.Point(22, 187);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(179, 13);
             this.linkLabel1.TabIndex = 3;
