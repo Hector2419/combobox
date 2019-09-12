e939d00e6b26c240b79213f7c9e46a99118d1083
Alumno <Alumno@PC20>
2019-09-12 09:20:08 -0500

    4.0

--- Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
+++ Proyecto 1.0/Proyecto 1.0/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +57,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(191, 60);
+            this.label2.Location = new System.Drawing.Point(248, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
@@ -65,7 +66,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(333, 60);
+            this.label3.Location = new System.Drawing.Point(393, 60);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 2;
@@ -74,7 +75,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(24, 224);
+            this.linkLabel1.Location = new System.Drawing.Point(12, 190);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(179, 13);
             this.linkLabel1.TabIndex = 3;
@@ -83,14 +84,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 131);
+            this.textBox1.Location = new System.Drawing.Point(12, 136);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(118, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(305, 89);
+            this.textBox3.Location = new System.Drawing.Point(365, 89);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(120, 20);
             this.textBox3.TabIndex = 6;
@@ -103,7 +104,7 @@
             this.opcionesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(437, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(503, 24);
             this.menuStrip1.TabIndex = 7;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -132,7 +133,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(160, 89);
+            this.comboBox2.Location = new System.Drawing.Point(217, 89);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(118, 21);
             this.comboBox2.TabIndex = 9;
@@ -141,7 +142,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(160, 131);
+            this.textBox2.Location = new System.Drawing.Point(217, 136);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(118, 20);
             this.textBox2.TabIndex = 10;
@@ -149,7 +150,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(106, 177);
+            this.button1.Location = new System.Drawing.Point(136, 136);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
@@ -157,11 +158,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(136, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 43);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "<----->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 261);
+            this.ClientSize = new System.Drawing.Size(503, 220);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.comboBox2);
@@ -199,6 +212,7 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Proyecto 1.0/Proyecto 1.0/Form1.cs
+++ Proyecto 1.0/Proyecto 1.0/Form1.cs
@@ -46,28 +46,35 @@ namespace Proyecto_1._0
 
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Button2_Click(object sender, EventArgs e)
         {
-            double o,res;
+            string i, t;
+            //string selectedItem = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            i = comboBox1.Text;
+            t = comboBox2.Text;
+            comboBox2.Text = i;
+            comboBox1.Text = t;
+            textBox3.Clear();
+            double o, res;
             string selectedItem = comboBox1.Items[comboBox1.SelectedIndex].ToString();
             coin = comboBox1.Text;
             op = comboBox2.Text;
-            n = float.Parse(textBox1.Text); 
+            n = float.Parse(textBox1.Text);
             if (coin == "Dollar USD")
             {
                 if (op == "Dollar USD")
-                {                    
+                {
                     result = n.ToString();
                     textBox2.Text = result;
                     textBox3.Text = "1 ---> 1";
                 }
-                else if (op== "Mexican Peso MXN")
+                else if (op == "Mexican Peso MXN")
                 {
                     o = 19.51;
-                    res = n*o;
+                    res = n * o;
                     result = res.ToString();
                     textBox2.Text = result;
-                    textBox3.Text = "1 ---> " + o;   
+                    textBox3.Text = "1 ---> " + o;
                 }
                 else if (op == "Euro EUR")
                 {
@@ -216,7 +223,7 @@ namespace Proyecto_1._0
                     textBox3.Text = "1 ---> " + o;
                 }
                 else if (op == "Libra GBP")
-                { 
+                {
                     o = 0.894153;
                     res = n * o;
                     result = res.ToString();
@@ -403,7 +410,415 @@ namespace Proyecto_1._0
                     res = n * o;
                     result = res.ToString();
                     textBox2.Text = result;
-                    textBox3.Text = "1 ---> " + o;  
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 24.0946;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 1.11838;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 132.856;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 4.13192;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 88.7889;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double o, res;
+            string selectedItem = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            coin = comboBox1.Text;
+            op = comboBox2.Text;
+            n = float.Parse(textBox1.Text);
+            if (coin == "Dollar USD")
+            {
+                if (op == "Dollar USD")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 19.51;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 0.91;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 107.43;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 3.34;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 71.87;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.809251;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Mexican Peso MXN")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 0.051;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 0.0464174;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 5.52;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 0.17;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 3.68647;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.0415030;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Euro EUR")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 1.10;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 21.5436;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 118.79;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 3.69;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 79.386;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.894153;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Yen JPY")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 0.0093;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 0.18132;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 0.00842;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 0.031070;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 0.668590;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.007529;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Soles PEN")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 0.299081;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 5.83099;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 0.270749;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 32.1631;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Rupias INR")
+                {
+                    o = 21.4953;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.242018;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Rupias INR")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 0.0139140;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Mexican Peso MXN")
+                {
+                    o = 0.271263;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Euro EUR")
+                {
+                    o = 0.0125967;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Yen JPY")
+                {
+                    o = 1.49568;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Soles PEN")
+                {
+                    o = 0.0465217;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+                else if (op == "Rupias INR")
+                {
+                    result = n.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> 1";
+                }
+                else if (op == "Libra GBP")
+                {
+                    o = 0.0112627;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
+                }
+            }
+            else if (coin == "Libra GBP")
+            {
+                if (op == "Dollar USD")
+                {
+                    o = 1.23571;
+                    res = n * o;
+                    result = res.ToString();
+                    textBox2.Text = result;
+                    textBox3.Text = "1 ---> " + o;
                 }
                 else if (op == "Mexican Peso MXN")
                 {
@@ -456,7 +871,12 @@ namespace Proyecto_1._0
 
         private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
+            
+             
+            
         }
     }
+        
+    
 }
